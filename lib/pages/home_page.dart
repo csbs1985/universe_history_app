18fc35884086f6e70ea9b3d29b55b62bdebd97b1
Charles Santos <charles.santos@sennit.com.br>
2022-01-07 15:42:25 -0300

    save homePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,87 +1,59 @@
-// ignore_for_file: unnecessary_new, prefer_const_constructors
+// ignore_for_file: unnecessary_new, prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, use_key_in_widget_constructors, must_be_immutable
 
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
-import 'package:universe_history_app/components/history_item_component.dart';
+import 'package:scroll_app_bar/scroll_app_bar.dart';
+import 'package:universe_history_app/components/call_create_component.dart';
 import 'package:universe_history_app/shared/models/history_model.dart';
 import 'package:universe_history_app/theme/ui_colors.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
-  late ScrollController _scrollViewController;
-  bool _showAppbar = true;
-  bool isScrollingDown = false;
+class HomePage extends StatelessWidget {
+  final _scrollController = ScrollController();
   List<HistoryModel> allHistory = HistoryModel.allHistory;
-
-  @override
-  void initState() {
-    super.initState();
-    _scrollViewController = new ScrollController();
-    _scrollViewController.addListener(() {
-      if (_scrollViewController.position.userScrollDirection ==
-          ScrollDirection.reverse) {
-        if (!isScrollingDown) {
-          isScrollingDown = true;
-          _showAppbar = false;
-          setState(() {});
-        }
-      }
-
-      if (_scrollViewController.position.userScrollDirection ==
-          ScrollDirection.forward) {
-        if (isScrollingDown) {
-          isScrollingDown = false;
-          _showAppbar = true;
-          setState(() {});
-        }
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _scrollViewController.dispose();
-    _scrollViewController.removeListener(() {});
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(
-        systemNavigationBarColor: uiColor.comp_5,
-        systemNavigationBarIconBrightness: Brightness.dark,
+        systemNavigationBarColor: uiColor.second,
+        systemNavigationBarIconBrightness: Brightness.light,
       ),
     );
 
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            AnimatedContainer(
-              height: _showAppbar ? 56.0 : 0.0,
-              duration: Duration(milliseconds: 200),
-              child: AppBar(
-                title: Text('Scroll Demo'),
-                actions: const [
-                  Text('title'),
-                ],
-              ),
+      appBar: ScrollAppBar(
+        controller: _scrollController, // Note the controller here
+        title: Text("App Bar"),
+      ),
+      body: Snap(
+        controller: _scrollController.appBar, // Controller is also here
+        child: Container(
+          child: SingleChildScrollView(
+            controller: _scrollController,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: 200,
+                  child: CallCreate(),
+                ),
+                Flexible(
+                  child: ListView.builder(
+                    itemCount: 15,
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) => ListTile(
+                      title: Text("List $index"),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              // controller: _scrollViewController,
-              child: HistoryItemComponent(allHistory),
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
 }
+//HistoryItemComponent(allHistory),
+//CallCreate(),
