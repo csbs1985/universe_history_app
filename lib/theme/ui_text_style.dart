9173993a0280b544171eb702a0d5096580c47c04
Charles Santos <charles.santos@sennit.com.br>
2022-02-17 08:20:41 -0300

    atualizar fontSize

--- lib/theme/ui_text_style.dart
+++ lib/theme/ui_text_style.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: camel_case_types, constant_identifier_names
+// ignore_for_file: camel_case_types
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -6,116 +6,115 @@ import 'package:universe_history_app/theme/ui_color.dart';
 
 class uiTextStyle {
   static const TextStyle btnFlex = TextStyle(
-    fontSize: 20,
+    fontSize: 16,
     fontWeight: FontWeight.w400,
     color: uiColor.third,
   );
 
   static const TextStyle btnFlexActived = TextStyle(
-    fontSize: 20,
+    fontSize: 16,
     fontWeight: FontWeight.w400,
     color: uiColor.comp_1,
   );
 
   static const TextStyle btn = TextStyle(
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: FontWeight.w400,
     color: uiColor.first,
   );
 
   static const TextStyle btnDisabled = TextStyle(
-    fontSize: 14,
+    fontSize: 12,
     fontWeight: FontWeight.w400,
     color: uiColor.comp_3,
   );
 
-  ///////////////////
   static const TextStyle header1 = TextStyle(
-    fontSize: 28,
-    fontWeight: FontWeight.w700,
-    color: uiColor.third,
-  );
-
-  static const TextStyle header2 = TextStyle(
     fontSize: 24,
     fontWeight: FontWeight.w700,
     color: uiColor.third,
   );
 
-  static const TextStyle header3 = TextStyle(
+  static const TextStyle header2 = TextStyle(
     fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: uiColor.third,
+  );
+
+  static const TextStyle header3 = TextStyle(
+    fontSize: 16,
     fontWeight: FontWeight.w700,
     color: uiColor.first,
   );
 
   static const TextStyle text1 = TextStyle(
-    fontSize: 20,
+    fontSize: 16,
     fontWeight: FontWeight.w400,
     color: uiColor.third,
   );
 
   static const TextStyle text2 = TextStyle(
-    fontSize: 16,
+    fontSize: 14,
     fontWeight: FontWeight.w400,
     color: uiColor.comp_2,
   );
 
   static const TextStyle text3 = TextStyle(
-    fontSize: 20,
+    fontSize: 16,
     fontWeight: FontWeight.w400,
     color: uiColor.first,
   );
 
   static const TextStyle text4 = TextStyle(
-    fontSize: 16,
+    fontSize: 14,
     fontWeight: FontWeight.w400,
     color: uiColor.third,
   );
 
   static const TextStyle text5 = TextStyle(
-    fontSize: 20,
+    fontSize: 16,
     fontWeight: FontWeight.w700,
     color: uiColor.third,
   );
 
   static const TextStyle text6 = TextStyle(
-    fontSize: 16,
+    fontSize: 14,
     fontWeight: FontWeight.w400,
     color: uiColor.first,
   );
 
   static const TextStyle text7 = TextStyle(
-    fontSize: 20,
+    fontSize: 16,
     fontWeight: FontWeight.w400,
     color: uiColor.comp_2,
   );
 
   static const TextStyle text8 = TextStyle(
-    fontSize: 20,
+    fontSize: 16,
     fontWeight: FontWeight.w400,
     color: uiColor.comp_1,
   );
 
   static const TextStyle buttonPrimary = TextStyle(
-    fontSize: 20,
+    fontSize: 16,
     fontWeight: FontWeight.w400,
     color: uiColor.third,
   );
 
   static const TextStyle button2 = TextStyle(
-    fontSize: 20,
+    fontSize: 16,
     fontWeight: FontWeight.w400,
     color: uiColor.third,
   );
 
   static const TextStyle chip = TextStyle(
-    fontSize: 20,
+    fontSize: 16,
     fontWeight: FontWeight.w400,
     color: uiColor.third,
   );
 
   static const TextStyle chipActive = TextStyle(
-    fontSize: 20,
+    fontSize: 16,
     fontWeight: FontWeight.w400,
     color: uiColor.first,
   );
