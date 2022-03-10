d47185729296f9f79f1c7d79ca9fc3dbc7d05243
Charles Santos <charles.santos@sennit.com.br>
2022-03-10 08:06:25 -0300

    adicionar HistoryItemComponent no HistoryPage

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,42 +1,51 @@
-// ignore_for_file: prefer_typing_uninitialized_variables, unnecessary_new, unused_local_variable
+// ignore_for_file: unnecessary_new, avoid_print, prefer_const_constructors
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:universe_history_app/components/appbar_back_component.dart';
 import 'package:universe_history_app/components/comment_component.dart';
 import 'package:universe_history_app/components/history_item_component.dart';
+import 'package:universe_history_app/components/skeleton_history_item_component.dart';
 import 'package:universe_history_app/core/api.dart';
 
 class HistoryPage extends StatefulWidget {
   const HistoryPage({Key? key}) : super(key: key);
 
   @override
-  State<HistoryPage> createState() => _HistoryItemPageState();
+  State<HistoryPage> createState() => _HistoryPageState();
 }
 
-class _HistoryItemPageState extends State<HistoryPage> {
+class _HistoryPageState extends State<HistoryPage> {
   final Api api = new Api();
-  // final AsyncSnapshot<QuerySnapshot> snapshot;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // api.getHistory().then((result) {
-    //   snapshot = result;
-    // }).catchError((error) => print('ERROR:' + error));
-  }
 
   @override
   Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: api.getHistory(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return SkeletonHistoryItemComponent();
+            case ConnectionState.done:
+            default:
+              try {
+                return _history(context, snapshot);
+              } catch (error) {
+                return SkeletonHistoryItemComponent();
+              }
+          }
+        });
+  }
+
+  Widget _history(BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
     return Scaffold(
       appBar: const AppbarBackComponent(),
       body: SingleChildScrollView(
-        child: Stack(
+        child: Column(
           children: [
-            Text('data')
-            // HistoryItemComponent(context: context, snapshot: snapshot),
-            // CommentComponent(),
+            HistoryItemComponent(context: context, snapshot: snapshot),
+            // CommentComponent()
           ],
         ),
       ),
