023d67642aa6f2da00fa9666665cd8d4e0a99fe3
Charles Santos <charles.santos@sennit.com.br>
2022-03-14 08:21:22 -0300

    azul original

---
+++ lib/components/comment_list_component.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class CommentListComponent extends StatefulWidget {
+  const CommentListComponent({Key? key}) : super(key: key);
+
+  @override
+  State<CommentListComponent> createState() => _CommentListComponentState();
+}
+
+class _CommentListComponentState extends State<CommentListComponent> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
