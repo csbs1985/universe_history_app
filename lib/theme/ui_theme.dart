9c6bbc25745047534f5ccdd546036298e3658b8d
Charles Santos <csbs.conta@outlook.com>
2022-03-24 16:28:58 -0300

    trocar font por nunito

--- lib/theme/ui_theme.dart
+++ lib/theme/ui_theme.dart
@@ -6,7 +6,7 @@ import 'package:universe_history_app/theme/ui_color.dart';
 class uiTheme {
   static ThemeData theme1 = ThemeData(
     scaffoldBackgroundColor: uiColor.comp_1,
-    fontFamily: 'segoe',
+    fontFamily: 'nunito-regular',
     appBarTheme: const AppBarTheme(
       backgroundColor: uiColor.comp_1,
       elevation: 0,
