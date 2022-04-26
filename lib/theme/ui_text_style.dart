d06fdb47b03c12da693b1a8dead2ce004ed9711d
Charles Santos <csbs.conta@outlook.com>
2022-04-26 14:12:54 -0300

    aumentar fontes

--- lib/theme/ui_text_style.dart
+++ lib/theme/ui_text_style.dart
@@ -48,7 +48,7 @@ class uiTextStyle {
   );
 
   static const TextStyle text2 = TextStyle(
-    fontSize: 12,
+    fontSize: 14,
     fontWeight: FontWeight.w400,
     color: uiColor.comp_2,
   );
@@ -91,13 +91,13 @@ class uiTextStyle {
   );
 
   static const TextStyle menu = TextStyle(
-    fontSize: 12,
+    fontSize: 14,
     fontWeight: FontWeight.w400,
     color: uiColor.comp_2,
   );
 
   static const TextStyle menuActive = TextStyle(
-    fontSize: 12,
+    fontSize: 14,
     fontWeight: FontWeight.w900,
     color: uiColor.comp_1,
   );
@@ -115,7 +115,7 @@ class uiTextStyle {
   );
 
   static const TextStyle buttonPublish = TextStyle(
-    fontSize: 12,
+    fontSize: 14,
     fontWeight: FontWeight.w700,
     color: uiColor.buttonLabel,
   );
